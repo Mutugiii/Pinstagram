c3dada861322072e11aae92755c3636f28c3c3ea
mutugiii <mutugimutuma@gmail.com>
2020-03-01 22:27:33 +0300

    Define database models and initial migration

--- africa/models.py
+++ africa/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class CrudMethods:
+    '''Method class for Common methods'''
+    def save(self):
+        '''Function to save class to database'''
+        self.save()
+
+    def delete(self):
+        '''Function to delete class from database'''
+        self.delete()
+
+    def update(self, **kwargs):
+        '''Function to update the class in database'''
+        for key,value in kwargs.items():
+            setattr(self,key,value)
+            self.save()
+
+class Location(models.Model, CrudMethods):
+    '''Location model for the image location details'''
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model, CrudMethods):
+    '''Category Model containing the image category details'''
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+class Image(models.Model, CrudMethods):
+    '''Image Class containing the Image details'''
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
--- africa/tests.py
+++ africa/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
-# Create your tests here.
+class TestLocation(TestCase):
+    '''Test class to test location class'''
+    def setUp(self):
+        '''Function to prepare for every test case'''
+        self.test_location = Location('Nairobi')
+
+    def tearDown(self):
+        '''Function to clean up after every test case'''
+        Location.objects.all().delete()
