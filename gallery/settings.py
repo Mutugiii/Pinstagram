0792d46a1ad516fc29d9cb41a18ad8bcba45d5ce
mutugiii <mutugimutuma@gmail.com>
2020-03-02 10:27:44 +0300

    Edit database urls

--- gallery/settings.py
+++ gallery/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': 'gallery',
+        'USER': 'mutugi',
+        'PASSWORD': 'Mutugi',
     }
 }
 
